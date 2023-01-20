8d920c1efdcb48a878317610012ba4441eeaa08a
Diego Lima <diegoperes142@gmail.com>
2023-01-20 17:51:09 -0300

    publicacao

---
+++ js/about.e47f1ba0.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkmestrado_vuetify"]=self["webpackChunkmestrado_vuetify"]||[]).push([[443],{7750:function(t,u,e){e.r(u),e.d(u,{default:function(){return l}});var n=function(){var t=this;t._self._c;return t._m(0)},s=[function(){var t=this,u=t._self._c;return u("div",{staticClass:"about"},[u("h1",[t._v("This is an about page")])])}],a=e(1001),r={},i=(0,a.Z)(r,n,s,!1,null,null,null),l=i.exports}}]);
+//# sourceMappingURL=about.e47f1ba0.js.map
